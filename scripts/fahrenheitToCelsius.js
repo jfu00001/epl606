dda16634db156188651637fd02f236a9f9e3028b
Ubuntu <ubuntu@ip-172-31-29-171.us-west-2.compute.internal>
2018-10-28 18:29:08 +0000

    map import

---
+++ scripts/fahrenheitToCelsius.js
@@ -0,0 +1,4 @@
+// Transform fahrenheit to Celsius
+function fahrenheitToCelsius(temperature){
+	return (5/9) * (temperature-32);
+}
---
+++ scripts/runningClock.js
@@ -0,0 +1,27 @@
+// http://www.webestools.com/scripts_tutorials-code-source-7-display-date-and-time-in-javascript-real-time-clock-javascript-date-time.html
+// Display's current day month date time dynamicly.(moving clock)
+function date_time(id){
+        date = new Date;
+        year = date.getFullYear();
+        month = date.getMonth();
+        months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
+        d = date.getDate();
+        day = date.getDay();
+        days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+        h = date.getHours();
+        if(h<10){
+            h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10){
+            m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10){
+            s = "0"+s;
+        }
+        result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;
+        document.getElementById(id).innerHTML = result;
+        setTimeout('date_time("'+id+'");','1000');
+        return true;
+}
